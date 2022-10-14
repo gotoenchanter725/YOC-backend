ac3e806b1cad20d792f8cbb243f7ff713127ee1d
goldmicky1210 <goldmicky1210@gmail.com>
2022-10-14 22:27:55 +0900

    set initial architecture

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const router = require('./routes');
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
