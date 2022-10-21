800ed667dddb05700e454381f97c387e1c9e2542
goldmicky1210 <goldmicky1210@gmail.com>
2022-10-21 22:02:51 +0900

    .env update

--- models/index.js
+++ models/index.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+require("dotenv").config();
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+console.log(env);
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
