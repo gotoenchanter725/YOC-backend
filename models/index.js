bd18d6020d0d47510f9c317e30eef5156681660c
unknown <codingwizard0831@gmail.com>
2023-02-01 01:57:14 +0900

    Update sequilize-config

--- models/index.js
+++ models/index.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+require('dotenv').config();
+console.log(process.env.NODE_ENV);
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 let sequelize;
 if (config.use_env_variable) {
