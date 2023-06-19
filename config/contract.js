b20ac73bc2e252d7683458b81b49352e50d0891b
harrypotter0831 <creativemoderndev0831@gmail.com>
2023-06-20 03:39:50 +0900

    update the rpc url

--- config/contract.js
+++ config/contract.js
@@ -6,7 +6,7 @@ const ETH_NETWORK = {
     testnet: {
         // RPC_URL: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
         // CHAIN_ID: 5
-        RPC_URL: "https://rpc2.sepolia.org",
+        RPC_URL: "https://rpc.sepolia.org",
         CHAIN_ID: 11155111
     }
 }
